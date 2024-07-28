1b0a614ea09c2ee219d555cbfdf00b1aa40c19ed
ibrahimElbaz <ibrahim.elbaz.1011@gmail.com>
2024-07-28 23:38:13 +0300

    done edit Failure file

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,8 +1,9 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/core/errors/failures.dart';
+
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failures, List<BookModel>>> fetchBooks();
-  Future<Either<Failures, List<BookModel>>> fetchNewstBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBooks();
+  Future<Either<Failure, List<BookModel>>> fetchNewstBooks();
 }
