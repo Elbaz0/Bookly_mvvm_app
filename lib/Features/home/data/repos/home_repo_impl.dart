3db79b67a83776d3534fcbdbbfad38284216a098
ibrahimElbaz <ibrahim.elbaz.1011@gmail.com>
2024-07-28 23:36:30 +0300

    Create ServerFailure From Dio Error

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -3,24 +3,32 @@ import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   ApiService apiService;
   HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failures, List<BookModel>>> fetchNewstBooks() async {
-    var data = await apiService.get(
-        endpoint:
-            'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:programming');
-    List<BookModel> Books = [];
-    for (var item in data['items']) {
-      Books.add(BookModel.fromJson(item));
+  Future<Either<Failure, List<BookModel>>> fetchNewstBooks() async {
+    try {
+      var data = await apiService.get(
+          endpoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:programming');
+      List<BookModel> Books = [];
+      for (var item in data['items']) {
+        Books.add(BookModel.fromJson(item));
+      }
+      return right(Books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure('Try Again later ..! '));
     }
-    return right(Books);
   }
 
   @override
-  Future<Either<Failures, List<BookModel>>> fetchBooks() {
+  Future<Either<Failure, List<BookModel>>> fetchBooks() {
     throw UnimplementedError();
   }
 }
