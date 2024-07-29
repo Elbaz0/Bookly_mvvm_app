d396ef40d25d69cdfe0fc074b8b8e51395f0f15a
ibrahimElbaz <ibrahim.elbaz.1011@gmail.com>
2024-07-29 11:16:38 +0300

    create newest book cubit method

--- lib/Features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart
+++ lib/Features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<newestBooksState> {
-  NewestBooksCubit() : super(newestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(newestBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> FetchBooks() async {
+    emit(newestBooksLoading());
+
+    var result = await homeRepo.fetchNewstBooks();
+    result.fold((failur) {
+      emit(newestBooksFailure(failur.errMessage));
+    }, (Books) {
+      emit(newestBooksSuccess(Books));
+    });
+  }
 }
